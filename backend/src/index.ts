77798a97ce61ef16f0f99431c6b6888a4535e414
Bishwa7 <bishwa244@gmail.com>
2026-01-19 17:00:42 +0530

    Room logic added to ws server (client sends message to one room)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,68 @@
 import {WebSocketServer, WebSocket} from "ws";
 const wss = new WebSocketServer({ port: 8080 })
 
-let userCount = 0;
-// let allSockets : WebSocket[] = []
+
+interface IncomingMessage {
+  type: "join" | "chat";
+  payload?: {
+    roomId?: string;
+    message?: string;
+  };
+}
+
+
+let rooms: Record<string, Set<WebSocket>> = {};
+
 
 wss.on("connection", (socket) => {
+    let currentRoom: string | null = null;
 
-    // allSockets.push(socket)
-    userCount = userCount + 1;
-    console.log("user connected #" + userCount)
+    socket.on("message", (data) => {
+        try
+        {
+            let parsedData = JSON.parse(data.toString()) as IncomingMessage;
+            const { type, payload } = parsedData
 
-
-
-    socket.on("message", (message) => {
-        console.log("message received " + message.toString())
-        // socket.send(message.toString() + " : sent from server")
-
-
-        // for(let i=0; i< allSockets.length; i++)
-        // {
-        //     let s = allSockets[i]
-        //     s?.send(message.toString() + " : sent from server")
-        // }
-
-        wss.clients.forEach((client) => {
-            if (client.readyState === 1) {
-                client.send(message.toString() +" : sent from server");
+            if(!type || !payload){
+                return
             }
-        })
+
+            if(type === "join" && typeof payload.roomId === "string"){
+                currentRoom = payload.roomId
+
+                if(!rooms[currentRoom]) {
+                    rooms[currentRoom] = new Set<WebSocket>()
+                }
+
+                rooms[currentRoom]!.add(socket)
+                return;
+            }
+
+
+            if(type === "chat" && typeof payload.message === "string" && currentRoom)
+            {
+                rooms[currentRoom]?.forEach((client) => {
+                    if(client.readyState === WebSocket.OPEN){
+                        client.send(`${payload!.message}`)
+                    }
+                })
+            }
+        }
+        catch (error)
+        {
+            console.error("Failed to process message", error)
+        }
     })
 
 
+
     socket.on("close", () => {
-        console.log("user disconnected")
+        if(currentRoom && rooms[currentRoom]){
+            rooms[currentRoom]?.delete(socket)
+
+            if(rooms[currentRoom]?.size === 0){
+                delete rooms[currentRoom]
+            }
+        }
     })
 })
